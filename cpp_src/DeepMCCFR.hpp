ab83b13f68219c6c556ea946c258d6cc00bb5ace
Azerus96 <gencinski1996@gmail.com>
2025-09-24 05:43:14 +0300

    Update DeepMCCFR.hpp

--- cpp_src/DeepMCCFR.hpp
+++ cpp_src/DeepMCCFR.hpp
@@ -19,16 +19,14 @@ namespace ofc {
 
 using LogQueue = py::object;
 
-// === ИЗМЕНЕНИЕ: Константа для размера буфера результатов, должна совпадать с Python ===
-constexpr size_t MAX_PENDING_REQUESTS = 200 * 4; // NUM_CPP_WORKERS * 4
-
 class DeepMCCFR {
 public:
     DeepMCCFR(size_t action_limit,
+              size_t first_street_candidates,
+              size_t max_pending_requests,
               SharedReplayBuffer* policy_buffer, 
               SharedReplayBuffer* value_buffer, 
               InferenceRequestQueue* request_queue,
-              // === ИЗМЕНЕНИЕ: Принимаем указатель на массив и его ширину ===
               float* result_array,
               size_t result_row_size,
               LogQueue* log_queue);
@@ -41,12 +39,13 @@ private:
     SharedReplayBuffer* value_buffer_;
     
     InferenceRequestQueue* request_queue_;
-    // === ИЗМЕНЕНИЕ: Храним указатель на массив и его ширину ===
     float* result_array_;
     size_t result_row_size_;
     LogQueue* log_queue_;
 
     size_t action_limit_;
+    size_t first_street_candidates_;
+    size_t max_pending_requests_;
     std::mt19937 rng_;
     std::vector<float> dummy_action_vec_;
     
